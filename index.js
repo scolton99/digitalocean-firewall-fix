af971fe6dbac36a798e751aad06bb40c9eac092d
Spencer Colton <me@scolton.tech>
2023-06-06 20:58:03 -0500

    Fix service message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const { DIGITALOCEAN_API_KEY } = process.env;
 
 const main = async () => {
-  console.log(`##teamcity[setParameter name='env.LAST_IP' VALUE='new value'`);
+  console.log(`##teamcity[setParameter name='env.LAST_IP' VALUE='new value']`);
 };
 
 main();

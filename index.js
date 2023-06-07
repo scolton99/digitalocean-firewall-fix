f6378a5d94ca4308dd23c6ea4891744cb38f6ad5
Spencer Colton <me@scolton.tech>
2023-06-06 21:08:47 -0500

    Use TC API

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const fetch = require('node-fetch');
 
-const { DIGITALOCEAN_API_KEY } = process.env;
+const { TC_API_KEY, DIGITALOCEAN_API_KEY } = process.env;
 
 const main = async () => {
-  console.log(`##teamcity[setParameter name='env.LAST_IP' value='new value']`);
+  await fetch('https://ci.sdc.sx/app/rest/buildTypes/Local_DigitalOceanFirewallFix_Main/parameters/env.LAST_IP', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'text/plain',
+      'Authorization': `Bearer ${TC_API_KEY}`
+    },
+    body: 'new value'
+  });
+
+  // console.log(`##teamcity[setParameter name='env.LAST_IP' value='new value']`);
 };
 
 main();
